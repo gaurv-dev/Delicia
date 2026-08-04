6bbad88254db6dfae73cec192dd6074f0c6a0eb1
gaurv-web <gauravsoni1804@gmail.com>
2026-08-04 16:26:36 +0530

    Initial project commit

---
+++ delicia/src/main/java/com/gaurav/delicia/DeliciaApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.delicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciaApplication.class, args);
+	}
+
+}

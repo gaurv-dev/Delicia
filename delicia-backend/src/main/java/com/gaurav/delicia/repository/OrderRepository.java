2fb59d1de88ffc6a117b68bf42dda958d1d3d378
gaurv-web <gauravsoni1804@gmail.com>
2026-08-12 23:45:48 +0530

    Added backend files

--- delicia-backend/src/main/java/com/gaurav/delicia/repository/OrderRepository.java
+++ delicia-backend/src/main/java/com/gaurav/delicia/repository/OrderRepository.java
@@ -1,2 +1,15 @@
-public class OrderRepository {
+package com.gaurav.delicia.repository;
+
+import com.gaurav.delicia.model.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, String> {
+
+    List<Order> findByUserId(String userId);
+    List<Order> findByStatus(Order.OrderStatus status);
+    List<Order> findByUserIdAndStatus(String userId, Order.OrderStatus status);
 }

dfda0ede217f76c1fc0768aed344c7671d95a6c9
gaurv-web <gauravsoni1804@gmail.com>
2026-08-28 08:24:32 +0530

    add new files

--- delicia-frontend/src/App.jsx
+++ delicia-frontend/src/App.jsx
@@ -1,21 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  return (
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-  );
+    return <AppRoutes />;
 }
 
 export default App;
---
+++ delicia-frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+export default function AppRoutes() {
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
+}
